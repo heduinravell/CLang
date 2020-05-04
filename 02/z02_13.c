03d0eb74dc804d01d6351a475bdbe420fcd34f4c
Hedrav Bands <hr19ut@brocku.ca>
2020-05-04 15:59:39 -0400

    comments add about boolean

--- 02/z02_13.c
+++ 02/z02_13.c
@@ -11,16 +11,19 @@ printf("Informe 2 numeros inteiros pfv:\n");
 int num1;
 int num2;
 
+
 scanf("%d %d", &num1, &num2); //atribuindo nome aos bois
+// num1 = num2;
+// 0 == false; !0 = true
 
 if (num1 == num2){
-	printf("Sao iguais, claro\n");
+	printf("São iguais, claro\n");
 } else {
-	printf("Sao diferentes, obvio\n");
+	printf("São diferentes, óbvio\n");
 	if (num1 > num2){
-		printf("E digo mais... %d eh maior do que %d", num1, num2);
+		printf("E digo mais... %d é maior do que %d", num1, num2);
 	} else {
-		printf("Teimo em dizer que %d eh maior do que %d", num2, num1);
+		printf("Teimo em dizer que %d é maior do que %d", num2, num1);
 	} 	
 }
 	
