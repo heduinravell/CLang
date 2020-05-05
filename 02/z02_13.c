5c78399191a45f44ec20156a0e4440042b1df8c0
Hedrav Bands <hr19ut@brocku.ca>
2020-05-05 02:25:47 -0400

    parameters

--- 02/z02_13.c
+++ 02/z02_13.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 printf("Informe 2 numeros inteiros pfv:\n");
-//printf("Informe 2 numeros agooooora:\n");
+//printf("Informe 2 numeros agora:\n");
 
 int num1;
 int num2;
@@ -17,13 +17,13 @@ scanf("%d %d", &num1, &num2); //atribuindo nome aos bois
 // 0 == false; !0 = true
 
 if (num1 == num2){
-	printf("São iguais, claro\n");
+	printf("Sao iguais, claro\n");
 } else {
-	printf("São diferentes, óbvio\n");
+	printf("Sao diferentes, obvio\n");
 	if (num1 > num2){
-		printf("E digo mais... %d é maior do que %d", num1, num2);
+		printf("E digo mais... %d eh maior do que %d", num1, num2);
 	} else {
-		printf("Teimo em dizer que %d é maior do que %d", num2, num1);
+		printf("Teimo em dizer que %d eh maior do que %d", num2, num1);
 	} 	
 }
 	
---
+++ 02/z02_15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	int nPartido;
+	printf("%s", "Informe o numero do seu Partido:\n");
+	scanf("%d", &nPartido);
+	printf("%d\n", nPartido);
+
+return 0;
+}
