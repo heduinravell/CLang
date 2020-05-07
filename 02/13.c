befdb1b5c7ab390e31c3cedaef92ae79ef9ab759
Hedrav Bands <hr19ut@brocku.ca>
2020-05-07 04:27:03 -0400

    using the values

--- 02/13.c
+++ 02/13.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void){
     int a, b, sum;
-    printf("Enter 2 numbers:\n ");
+    printf("Enter 2 numbers: ");
     scanf("%d%d", &a, &b);
     sum = a + b;
-    printf("\nThe sum is %d from %d and %d.", &sum, &a, &b);
+    printf("\nThe sum is %d from %d and %d.", sum, a, b);
 return 0;
 }
